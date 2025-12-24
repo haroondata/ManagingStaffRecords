c89d98691b2cce362d6730b603af62761b48b940
Haroon <harooncloud4@gmail.com>
2025-12-24 23:26:53 +0000

    added frame to do main function

--- src/managingstaffrecords/app/Main.java
+++ src/managingstaffrecords/app/Main.java
@@ -1,11 +1,28 @@
 package managingstaffrecords.app;
 
-public class Main {
+import javax.swing.JFrame;
 
-	public static void MainApp (String[] args) {
+public class Main extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public static void main (String[] args) {
 		// TODO Auto-generated method stub
-		new MainFrame().setVisible(true);
+		Main frame = new Main();
+		frame.setVisible(true);
  
+	}
+	
+	public Main() {
+	    setTitle("Managing Staff Records");
+	    setSize(1200, 800);
+	    setLocationRelativeTo(null);
+	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	    // add panels, buttons, menus here
 	}
 
 }
