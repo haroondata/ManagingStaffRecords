17190a1465899eb9c336693cd412ddf159796335
Haroon <harooncloud4@gmail.com>
2025-12-24 20:58:05 +0000

    Initial commit

---
+++ src/managingstaffrecords/app/Main.java
@@ -0,0 +1,11 @@
+package managingstaffrecords.app;
+
+public class Main {
+
+	public static void MainApp (String[] args) {
+		// TODO Auto-generated method stub
+		new MainFrame().setVisible(true);
+
+	}
+
+}
